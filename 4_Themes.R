357be4b49f553d967242ac1e9b91eec3beb55492
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-06 18:30:44 -0800

    added code to 4_Themes R file

---
+++ 4_Themes.R
@@ -0,0 +1,304 @@
+# To change stylistic elements of a plot, call theme() and set plot properties to a new value.
+# aPlot + theme(legend.position = new_value)
+# Here, the new value can be:
+# - "top", "bottom", "left", or "right'": place it at that side of the plot.
+# - "none": don't draw it.
+# - c(x, y): c(0, 0) means the bottom-left and c(1, 1) means the top-right.
+
+# NOT LOADED HERE SO CODE WON'T WORK
+# View the default plot
+plt_prop_unemployed_over_time
+
+# Remove legend entirely
+plt_prop_unemployed_over_time +
+  theme(legend.position = "none")
+
+# Position the legend at the bottom of the plot
+plt_prop_unemployed_over_time +
+  theme(legend.position = "bottom")
+
+# Position the legend inside the plot at (0.6, 0.1)
+plt_prop_unemployed_over_time +
+  theme(legend.position = c(0.6, 0.1))
+
+
+# Line elements in the plot such as axes and gridlines have a color, a thickness (size), and a line type (solid line, dashed, or dotted). 
+# To set the style of a line, you use element_line(). For example, to make the axis lines into red, dashed lines, you would use the following:
+# aPlot + theme(axis.line = element_line(color = "red", linetype = "dashed"))
+# Similarly, element_rect() changes rectangles and element_text() changes text. 
+# You can remove a plot element using element_blank().
+
+plt_prop_unemployed_over_time +
+  theme(
+    # For all rectangles, set the fill color to grey92
+    rect = element_rect(fill = "grey92"),
+    # For the legend key, turn off the outline
+    legend.key = element_rect(color = NA)
+  )
+
+plt_prop_unemployed_over_time +
+  theme(
+    rect = element_rect(fill = "grey92"),
+    legend.key = element_rect(color = NA),
+    # Turn off axis ticks
+    axis.ticks = element_blank(),
+    # Turn off the panel grid
+    panel.grid = element_blank()
+  )
+
+plt_prop_unemployed_over_time +
+  theme(
+    rect = element_rect(fill = "grey92"),
+    legend.key = element_rect(color = NA),
+    axis.ticks = element_blank(),
+    panel.grid = element_blank(),
+    # Add major y-axis panel grid lines back
+    panel.grid.major.y = element_line(
+      # Set the color to white
+      color = "white",
+      # Set the size to 0.5
+      size = 0.5,
+      # Set the line type to dotted
+      linetype = "dotted"
+    )
+  )
+
+plt_prop_unemployed_over_time +
+  theme(
+    rect = element_rect(fill = "grey92"),
+    legend.key = element_rect(color = NA),
+    axis.ticks = element_blank(),
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(
+      color = "white",
+      size = 0.5,
+      linetype = "dotted"
+    ),
+    # Set the axis text color to grey25
+    axis.text = element_text(color = "grey25"),
+    # Set the plot title font face to italic and font size to 16
+    plot.title = element_text(face = "italic", size = 16)
+  )
+
+
+# Whitespace means all the non-visible margins and spacing in the plot.
+# To set a single whitespace value, use unit(x, unit), where x is the amount and unit is the unit of measure.
+# Borders require you to set 4 positions, so use margin(top, right, bottom, left, unit). To remember the margin order, think TRouBLe.
+# The default unit is "pt" (points), which scales well with text. Other options include "cm", "in" (inches) and "lines" (of text).
+
+# NOT LOADED HERE SO CODE WON'T WORK
+# View the original plot
+plt_mpg_vs_wt_by_cyl
+
+plt_mpg_vs_wt_by_cyl +
+  theme(
+    # Set the axis tick length to 2 lines
+    axis.ticks.length = unit(2, "lines")
+  )
+
+plt_mpg_vs_wt_by_cyl +
+  theme(
+    # Set the legend key size to 3 centimeters
+    legend.key.size = unit(3, "cm")
+  )
+
+plt_mpg_vs_wt_by_cyl +
+  theme(
+    # Set the legend margin to (20, 30, 40, 50) points
+    legend.margin = margin(20, 30, 40, 50, "pt")
+  )
+
+plt_mpg_vs_wt_by_cyl +
+  theme(
+    # Set the plot margin to (10, 30, 50, 70) millimeters
+    plot.margin = margin(10, 30, 50, 70, "mm")
+  )
+
+
+# USING BUILT-IN THEMES WITH theme_*()
+# Some of the built-in themes:
+# theme_gray() is the default.
+# theme_bw() is useful when you use transparency; black and white
+# theme_classic() is more traditional.
+# theme_void() removes everything but the data.
+
+# NOT LOADED HERE SO CODE WON'T WORK
+# Add a black and white theme
+plt_prop_unemployed_over_time +
+  theme_bw()
+
+# Add a classic theme
+plt_prop_unemployed_over_time +
+  theme_classic()
+
+# Add a void theme
+plt_prop_unemployed_over_time +
+  theme_void()
+
+
+# ggthemes() PACKAGE
+install.packages("ggthemes")
+library(ggthemes)
+
+# NOT LOADED HERE SO CODE WON'T WORK
+# Use the fivethirtyeight theme
+plt_prop_unemployed_over_time +
+  theme_fivethirtyeight()
+
+# Use Tufte's theme
+plt_prop_unemployed_over_time +
+  theme_tufte()
+
+# Use the Wall Street Journal theme
+plt_prop_unemployed_over_time +
+  theme_wsj()
+
+
+# SETTING THEMES
+# Two options:
+# - Assign the theme to a variable, and add it to each plot.
+# - Set your theme as the default using theme_set().
+
+# Theme layer saved as an object, theme_recession
+theme_recession <- theme(
+  rect = element_rect(fill = "grey92"),
+  legend.key = element_rect(color = NA),
+  axis.ticks = element_blank(),
+  panel.grid = element_blank(),
+  panel.grid.major.y = element_line(color = "white", size = 0.5, linetype = "dotted"),
+  axis.text = element_text(color = "grey25"),
+  plot.title = element_text(face = "italic", size = 16),
+  legend.position = c(0.6, 0.1)
+)
+
+# Combine the Tufte theme with theme_recession
+theme_tufte_recession <- theme_tufte() + theme_recession
+
+# NOT LOADED HERE SO CODE WON'T WORK
+# Add the recession theme to the plot
+plt_prop_unemployed_over_time + theme_tufte_recession
+
+# Set theme_tufte_recession as the default theme
+theme_set(theme_tufte_recession)
+
+# Draw the plot (without explicitly adding a theme)
+plt_prop_unemployed_over_time
+
+plt_prop_unemployed_over_time +
+  # Add Tufte's theme
+  theme_tufte()
+
+plt_prop_unemployed_over_time +
+  theme_tufte() +
+  # Add individual theme elements
+  theme(
+    # Turn off the legend
+    legend.position = "none",
+    # Turn off the axis ticks
+    axis.ticks = element_blank()
+  )
+
+plt_prop_unemployed_over_time +
+  theme_tufte() +
+  theme(
+    legend.position = "none",
+    axis.ticks = element_blank(),
+    # Set the axis title's text color to grey60
+    axis.title = element_text(color = "grey60"),
+    # Set the axis text's text color to grey60
+    axis.text = element_text(color = "grey60")
+  )
+
+plt_prop_unemployed_over_time +
+  theme_tufte() +
+  theme(
+    legend.position = "none",
+    axis.ticks = element_blank(),
+    axis.title = element_text(color = "grey60"),
+    axis.text = element_text(color = "grey60"),
+    # Set the panel gridlines major y values
+    panel.grid.major.y = element_line(
+      # Set the color to grey60
+      color = "grey60",
+      # Set the size to 0.25
+      size = 0.25,
+      # Set the linetype to dotted
+      linetype = "dotted"
+    )
+  )
+
+
+# EXPLANATORY PLOT
+
+# NOT LOADED HERE SO CODE WON'T WORK
+# Add a geom_segment() layer
+ggplot(gm2007, aes(x = lifeExp, y = country, color = lifeExp)) +
+  geom_point(size = 4) +
+  geom_segment(aes(xend = 30, yend = country), size = 2)
+
+# Add a geom_text() layer
+ggplot(gm2007, aes(x = lifeExp, y = country, color = lifeExp)) +
+  geom_point(size = 4) +
+  geom_segment(aes(xend = 30, yend = country), size = 2) +
+  geom_text(aes(label = lifeExp), color = "white", size = 1.5)
+
+# Set the color scale
+palette <- brewer.pal(5, "RdYlBu")[-(2:4)]
+
+# Modify the scales
+ggplot(gm2007, aes(x = lifeExp, y = country, color = lifeExp)) +
+  geom_point(size = 4) +
+  geom_segment(aes(xend = 30, yend = country), size = 2) +
+  geom_text(aes(label = round(lifeExp,1)), color = "white", size = 1.5) +
+  scale_x_continuous("", expand = c(0,0), limits = c(30,90), position = "top") +
+  scale_color_gradientn(colors = palette)
+
+# Set the color scale
+palette <- brewer.pal(5, "RdYlBu")[-(2:4)]
+
+# Add a title and caption
+ggplot(gm2007, aes(x = lifeExp, y = country, color = lifeExp)) +
+  geom_point(size = 4) +
+  geom_segment(aes(xend = 30, yend = country), size = 2) +
+  geom_text(aes(label = round(lifeExp,1)), color = "white", size = 1.5) +
+  scale_x_continuous("", expand = c(0,0), limits = c(30,90), position = "top") +
+  scale_color_gradientn(colors = palette) +
+  labs(title = "Highest and lowest life expectancies, 2007", caption = "Source: gapminder")
+
+# Define the theme
+plt_country_vs_lifeExp +
+  theme_classic() +
+  theme(axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text = element_text(color = "black"),
+        axis.title = element_blank(),
+        legend.position = "none")
+
+# Add a vertical line
+plt_country_vs_lifeExp +
+  step_1_themes +
+  geom_vline(xintercept = global_mean, color = "grey40", linetype = 3)
+
+# Add text
+plt_country_vs_lifeExp +
+  step_1_themes +
+  geom_vline(xintercept = global_mean, color = "grey40", linetype = 3) +
+  annotate(
+    "text",
+    x = x_start, y = y_start,
+    label = "The\nglobal\naverage",
+    vjust = 1, size = 3, color = "grey40"
+  )
+
+# Add a curve
+plt_country_vs_lifeExp +
+  step_1_themes +
+  geom_vline(xintercept = global_mean, color = "grey40", linetype = 3) +
+  step_3_annotation +
+  annotate(
+    "curve",
+    x = x_start, y = y_start,
+    xend = x_end, yend = y_end,
+    arrow = arrow(length = unit(0.2, "cm"), type = "closed"),
+    color = "grey40"
+  )
